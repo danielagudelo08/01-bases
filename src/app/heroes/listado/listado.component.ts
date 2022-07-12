fa4e3a2ecf0bd367529ddc37f49050adc467d4e0
DANIEL AGUDELO MONTOYA <dagudelo483@misena.edu.co>
2022-07-12 15:05:16 -0500

    Angular

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman','Superman','IronMan','Thor'];
+  heroeBorrado: string = '';
+
+  borrarHeroe(){
+
+     this.heroeBorrado = this.heroes.shift() || '';
+  }
+
+
+
+}
